9236d6bcb9833871008fb9ef3400fd35c8494463
Dmytro <dimores95@gmail.com>
2023-03-21 16:07:58 +0200

    built-in modal window component

---
+++ src/components/Modal.vue
@@ -0,0 +1,72 @@
+<template>
+   <div class="modal">
+      <div class="modal__overlay" @click="close">
+         <div class="modal__body" @click.stop>
+            <div class="modal__header">
+               <slot name="header"> default header </slot>
+            </div>
+            <div class="modal__main">
+               <slot> default body </slot>
+            </div>
+            <div class="modal__footer">
+               <slot name="footer">
+                  <button @click="close">OK</button>
+               </slot>
+            </div>
+         </div>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'modal',
+   emits: {
+      close: null
+   },
+   methods: {
+      close() {
+         this.$emit('close');
+      }
+   }
+};
+</script>
+
+<style lang="less" scoped>
+.modal {
+   position: fixed;
+   top: 0;
+   left: 0;
+   width: 100%;
+   height: 100vh;
+
+   &__overlay {
+      position: absolute;
+      left: 0;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      background: #000000af;
+      padding: 20px;
+      display: flex;
+      align-items: center;
+   }
+
+   &__body {
+      background: #fefefe;
+      max-width: 600px;
+      border-radius: 12px;
+      padding: 20px;
+      border: 1px solid #888;
+      margin-left: auto;
+      margin-right: auto;
+      flex: 1 1 auto;
+   }
+
+   &__footer {
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+   }
+}
+</style>
--- src/components/TableCurrencyCourses.vue
+++ src/components/TableCurrencyCourses.vue
@@ -21,17 +21,39 @@
             </tbody>
          </table>
       </div>
+
+      <div class="group-btn">
+         <button class="btn btn-outline-primary" @click="add">Add</button>
+         <button
+            class="btn btn-outline-primary"
+            :class="isUpdate ? 'disabled' : ''"
+            @click="update"
+            :disabled="isUpdate">
+            Update ({{ timeToUpdate }})
+         </button>
+      </div>
+      <transition name="modal">
+         <modal v-if="showModal" @close="closeModal"></modal>
+      </transition>
    </div>
 </template>
 
 <script>
+import Modal from '@/components/Modal.vue';
 import { getCyrency, getFiatCyrencys } from '../api/cryptocompareApi.js';
 export default {
    name: 'TableCourses',
+   components: {
+      Modal
+   },
    data() {
       return {
          currency: 'USD',
-         currencies: null
+         currencies: null,
+         isUpdate: false,
+         timeToUpdate: 5,
+         timerId: null,
+         showModal: false
       };
    },
    computed: {
@@ -55,11 +77,37 @@ export default {
          return table;
       }
    },
-   methods: {},
+   methods: {
+      add() {
+         this.showModal = true;
+      },
+      closeModal() {
+         this.showModal = false;
+      },
+      update() {
+         getCyrency().then((response) => {
+            this.currencies = response;
+         });
+
+         this.isUpdate = true;
+         this.timeToUpdate = 5;
+         this.timerId = setInterval(() => {
+            this.timeToUpdate--;
+         }, 1000);
+      }
+   },
    mounted() {
       getCyrency().then((response) => {
          this.currencies = response;
       });
+   },
+   watch: {
+      timeToUpdate() {
+         if (!this.timeToUpdate) {
+            clearInterval(this.timerId);
+            this.isUpdate = false;
+         }
+      }
    }
 };
 </script>
@@ -100,6 +148,73 @@ export default {
    }
 }
 
+.group-btn {
+   text-align: center;
+   margin-top: 20px;
+}
+
+.btn {
+   display: inline-block;
+   font-weight: 400;
+   color: #212529;
+   text-align: center;
+   vertical-align: middle;
+   -webkit-user-select: none;
+   -moz-user-select: none;
+   -ms-user-select: none;
+   user-select: none;
+   background-color: transparent;
+   border: 1px solid transparent;
+   padding: 0.375rem 0.75rem;
+   font-size: 1rem;
+   line-height: 1.5;
+   border-radius: 0.25rem;
+   margin: 0 10px;
+   transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+      border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+
+.btn:not(:disabled):not(.disabled) {
+   cursor: pointer;
+}
+
+.btn-outline-primary {
+   color: #007bff;
+   border-color: #007bff;
+}
+
+.btn-outline-primary:hover {
+   color: #fff;
+   background-color: #007bff;
+   border-color: #007bff;
+}
+
+.btn-outline-primary:not(:disabled):not(.disabled).active:focus,
+.btn-outline-primary:not(:disabled):not(.disabled):active:focus,
+.show > .btn-outline-primary.dropdown-toggle:focus {
+   box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.5);
+}
+
+.btn-outline-primary:not(:disabled):not(.disabled).active,
+.btn-outline-primary:not(:disabled):not(.disabled):active,
+.show > .btn-outline-primary.dropdown-toggle {
+   color: #fff;
+   background-color: #007bff;
+   border-color: #007bff;
+}
+
+[type='button']:not(:disabled),
+[type='reset']:not(:disabled),
+[type='submit']:not(:disabled),
+button:not(:disabled) {
+   cursor: pointer;
+}
+
+.disabled {
+   cursor: unset;
+   opacity: 0.7;
+}
+
 @media (max-width: 380px) {
    .currency-select {
       width: 260px;
@@ -111,5 +226,16 @@ export default {
          min-width: unset;
       }
    }
+}
+
+.modal-enter-from,
+.modal-leave-to {
+   opacity: 0;
+}
+
+.modal-enter-active .modal-container,
+.modal-leave-active .modal-container {
+   -webkit-transform: scale(1.1);
+   transform: scale(1.1);
 }
 </style>
